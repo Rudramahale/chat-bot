71f73a8aa6d7d737ae5e217b5f158bcf9bc7247e
Rudramahale <rudramahale1@gamil.com>
2025-12-23 16:54:42 +0530

    new commit

---
+++ api/predict.py
@@ -0,0 +1,30 @@
+import os 
+import joblib
+import numpy as np 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+model_path = os.path.join(BASE_DIR,"models","intent_classifier.pkl")
+vectorizer_path = os.path.join(BASE_DIR,"models", "tf-idf_vectorizer.pkl")
+intent_classifier_path = os.path.join(BASE_DIR,"models", "id_to_intent.pkl")
+
+
+
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
+id_to_intent = joblib.load(intent_classifier_path)
+
+def predict_intent(user_message):
+    
+    x = vectorizer.transform([user_message])
+
+    probs = model.predict_proba(x)[0]
+    confidence = float(np.max(probs))
+
+    intent_id = int(np.argmax(probs))
+    intent = id_to_intent[intent_id]
+
+    return intent, confidence
+
+print(predict_intent("ye work nahi ho raha"))
